9ecaa0dfc72753dfc98100ec7d98e9664739d5f2
k.dlr <kenji.delarosa@alten.com>
2022-09-15 08:11:55 +0200

    adc: reorder error bit

--- src/hdl/fpasim/adc/adc_top.vhd
+++ src/hdl/fpasim/adc/adc_top.vhd
@@ -305,9 +305,8 @@ begin
       );
   end generate gen_errors_latch;
 
-  o_errors(15 downto 5) <= (others => '0');
-  o_errors(4)           <= error_tmp_bis(2); -- fifo rst error
-  o_errors(3 downto 2)  <= (others => '0');
+  o_errors(15 downto 3) <= (others => '0');
+  o_errors(2)           <= error_tmp_bis(2); -- fifo rst error
   o_errors(1)           <= error_tmp_bis(1); -- fifo rd empty error
   o_errors(0)           <= error_tmp_bis(0); -- fifo wr full error
 
@@ -318,7 +317,6 @@ begin
   -- for simulation only
   ---------------------------------------------------------------------
   assert not (error_tmp_bis(2) = '1') report "[adc_top] => FIFO is used before the end of the initialization " severity error;
-
   assert not (error_tmp_bis(1) = '1') report "[adc_top] => FIFO read an empty FIFO" severity error;
   assert not (error_tmp_bis(0) = '1') report "[adc_top] => FIFO write a full FIFO" severity error;
 
