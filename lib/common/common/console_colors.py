2013d212324c3c50ed7377474bcb4e6c80ddb7a3
k.dlr <kenji.delarosa@alten.com>
2022-10-13 12:22:01 +0200

    ci: add common python library

---
+++ lib/common/common/console_colors.py
@@ -0,0 +1,49 @@
+# -------------------------------------------------------------------------------------------------------------
+#                              Copyright (C) 2022-2030 Ken-ji de la Rosa, IRAP Toulouse.
+# -------------------------------------------------------------------------------------------------------------
+#                              This file is part of the ATHENA X-IFU DRE Focal Plane Assembly simulator.
+#
+#                              fpasim-fw is free software: you can redistribute it and/or modify
+#                              it under the terms of the GNU General Public License as published by
+#                              the Free Software Foundation, either version 3 of the License, or
+#                              (at your option) any later version.
+#
+#                              This program is distributed in the hope that it will be useful,
+#                              but WITHOUT ANY WARRANTY; without even the implied warranty of
+#                              MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#                              GNU General Public License for more details.
+#
+#                              You should have received a copy of the GNU General Public License
+#                              along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# -------------------------------------------------------------------------------------------------------------
+#    email                   kenji.delarosa@alten.com
+#    @file                   console_colors.py
+# -------------------------------------------------------------------------------------------------------------
+#    Automatic Generation    No
+#    Code Rules Reference    N/A
+# -------------------------------------------------------------------------------------------------------------
+#    @details                
+#    This script defines console print colors
+# -------------------------------------------------------------------------------------------------------------
+
+class ConsoleColors(object):
+    '''
+    This class define a set of colors (class attribute) with its associated string color code
+    If used in a python print function ( at the beginning of a string message), it colors the message
+    '''
+    black = '\033[30m'
+    red = '\033[31m'
+    green = '\033[32m'
+    orange = '\033[33m'
+    blue = '\033[34m'
+    purple = '\033[35m'
+    cyan = '\033[36m'
+    lightgrey = '\033[37m'
+    darkgrey = '\033[90m'
+    lightred = '\033[91m'
+    lightgreen = '\033[92m'
+    yellow = '\033[93m'
+    lightblue = '\033[94m'
+    pink = '\033[95m'
+    lightcyan = '\033[96m'
+    reset = '\033[0m'
