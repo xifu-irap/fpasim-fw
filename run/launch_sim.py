7278914403b788cafb6a0c83b894767936fca407
k.dlr <kenji.delarosa@alten.com>
2022-10-20 09:49:50 +0200

    ci: add the --tb_filename, --wave_filename, --conf_filename_list command lines. Enhance the console print

--- run/launch_sim.py
+++ run/launch_sim.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # -------------------------------------------------------------------------------------------------------------
 #                              Copyright (C) 2022-2030 Ken-ji de la Rosa, IRAP Toulouse.
 # -------------------------------------------------------------------------------------------------------------
@@ -36,9 +37,6 @@ import subprocess
 # Json lib
 import json
 
-# get the script name (without file extension)
-script_name = str(Path(__file__).stem)
-
 
 # retrieve all python library path in order to import them
 def find_file_in_hierarchy(filename_p='DONT_DELETE.txt', depth_level_p=10):
@@ -47,15 +45,15 @@ def find_file_in_hierarchy(filename_p='DONT_DELETE.txt', depth_level_p=10):
     If found, it returns the base path of the filename_p as well as its corresponding filepath
     :param filename_p: (string) filename to search in the hierarchy
     :param depth_level_p: (integer >= 0) search depth limit
-    :return: basepath, filepath: (string, string) (basepath of filename_p, filepath of filename_p) if found.
+    :return: base_path, filepath: (string, string) (base_path of filename_p, filepath of filename_p) if found.
                                                   Otherwise (None, None)
     """
     script_name0 = str(Path(__file__).stem)
     start_path = Path(__file__)
     ok = 0
     for i in range(depth_level_p):
-        basepath = str(start_path.parents[i])
-        filepath = str(Path(basepath, filename_p).resolve())
+        base_path = str(start_path.parents[i])
+        filepath = str(Path(base_path, filename_p).resolve())
         exist = Path(filepath).is_file()
         if exist == True:
             ok = 1
@@ -63,24 +61,38 @@ def find_file_in_hierarchy(filename_p='DONT_DELETE.txt', depth_level_p=10):
         else:
             continue
     if ok == 1:
-        return basepath, filepath
+        return base_path, filepath
     else:
-        msg = '[' + script_name0 + '] : ERRORS: the filename :' + filename_p + "wasn't found in the script hierarchy of " + str(start_path)
-        print(msg)
+        str0 = '[' + script_name0 + '] : ERRORS: the filename :' + filename_p + "wasn't found in the script hierarchy of " + str(
+            start_path)
+        print(str0)
         return None, None
 
 
+############################################################################
+# get the Project Root Path
+#  because the DONT_DELETE.txt file is saved in the git repo root directory
+############################################################################
+root_path, _ = find_file_in_hierarchy()
+
+############################################################################
+# add python common library
+############################################################################
+path = str(Path(root_path, 'lib/common'))
+sys.path.append(path)
+from common import Display
+
 if __name__ == '__main__':
 
-    ####################################
+    ############################################################################
     # parse command line
-    ####################################
+    ############################################################################
     parser = argparse.ArgumentParser(description='Launch some test_list')
     # add an optional argument with limited choices
     parser.add_argument('--simulator', '-s', default='questa', choices=['modelsim', 'questa'],
                         help='Specify the VHDL simulator to use (must be VHDL 2008 compatible).')
     # add an optional argument with a list of values
-    parser.add_argument('--test_name_list', '-t', default='test_tmp', metavar='test_name_list', nargs='*',
+    parser.add_argument('--test_name_list', '-t', default=['test0_tb_system_fpasim'], nargs='*',
                         help='define a list of test_list name to simulate. These test_list names are defined in the launch_sim.json file')
     # add an optional argument 
     parser.add_argument('--display', default='False', choices=['True', 'False'],
@@ -95,12 +107,25 @@ if __name__ == '__main__':
     test_name_list = args.test_name_list
     display = args.display
     verbosity = args.v
-    sep = os.sep
 
-    basepath, _ = find_file_in_hiearchy()
+    ############################################################################
+    # Search the Project Base Path: root of the git repository
+    ############################################################################
+    json_filepath_input = str(Path(root_path, 'launch_sim_processed.json').resolve())
 
-    json_filepath_input = str(Path(basepath, 'launch_sim_processed.json').resolve())
+    ############################################################################
+    # build the display object
+    ############################################################################
+    level0 = 0
+    level1 = 1
+    level2 = 2
+    obj_display = Display()
+    msg0 = 'Start Python Script: ' + __file__
+    obj_display.display_title(msg_p=msg0, level_p=level0)
 
+    ############################################################################
+    # get the file content
+    ############################################################################
     # get the root path of this file
     root_path = str(Path(__file__).parent)
 
@@ -114,31 +139,47 @@ if __name__ == '__main__':
     # Closing file
     fid_in.close()
 
-    ########################################
+    ############################################################################
     # display debug message
-    ########################################
+    ############################################################################
     if verbosity == 2:
-        h0 = '*' * 20
-        indent = ' ' * 4
-        print(h0)
-        print(indent + __file__ + ': processing in progress')
-        print(indent * 2 + 'input file: ' + json_filepath_input)
+        msg0 = 'Extract info from: ' + json_filepath_input
+        obj_display.display(msg_p=msg0, level_p=level1, color_p='green')
 
-    #########################################
-    # Execute the sequence of test_list
-    #########################################
+    ############################################################################
+    # Execute each individual test of a test_list
+    ############################################################################
     for test_name in test_name_list:
         test_list = json_data.get(test_name)
         if test_list is None:
-            print("ERROR: No test list: " + test_name)
+            msg0 = "ERROR: This " + test_name + " isn't defined in the json file (" + json_filepath_input +')'
+            obj_display.display(msg_p=msg0, level_p=level1, color_p='red')
             break
+        msg0 = 'Start: ' + test_name + ' test'
+        obj_display.display_subtitle(msg_p=msg0, level_p=level1, color_p='yellow')
         # search in test_list a name with test_name value
         for dic in test_list:
             vunit_file_path = str(Path(dic['vunit']['run_filepath']))
-            output_path = str(Path(dic['vunit']['output_path']))
+            output_path = str(Path(dic['vunit']['vunit_outpath']))
             report_path = str(Path(output_path, 'report.xml'))
-            json_project_path = str(Path(output_path, 'project_info.json'))
+            tb_filename = str(Path(dic['vunit']['tb_filename']))
+            wave_filename = str(Path(dic['vunit']['wave_filename']))
+            conf_filename_list = dic['conf']['filename_list']
 
+            msg_list = []
+            msg_list.append('vunit_file_path: ' + vunit_file_path)
+            msg_list.append('output_path    : ' + output_path)
+            msg_list.append('report_path    : ' + report_path)
+            msg_list.append('tb_filename    : ' + tb_filename)
+            msg_list.append('wave_filename  : ' + wave_filename)
+
+            for msg in msg_list:
+                obj_display.display(msg_p=msg, level_p=level2, color_p='green')
+
+            ############################################################################
+            # Call the python script
+            #  => Generate the list of argument
+            ############################################################################
             cmd = []
             # call python
             cmd.append('python')
@@ -156,7 +197,20 @@ if __name__ == '__main__':
             cmd.append('--display')
             cmd.append(display)
 
-            print('cmd_gui', display, 'type(cmd_gui)', type(display))
+            # specify the testbench name
+            cmd.append('--tb_filename')
+            cmd.append(tb_filename)
+            # specify the waveform name
+            cmd.append('--wave_filename')
+            cmd.append(wave_filename)
+
+            # If the conf_filename_list has values
+            #   => generate the command line
+            if conf_filename_list != []:
+                str0 = ' '.join(conf_filename_list)
+                cmd.append('--conf_filename_list')
+                cmd.append(str0)
+
 
             cmd.append('-o')
             cmd.append(output_path)
@@ -164,8 +218,8 @@ if __name__ == '__main__':
             cmd.append('-x')
             cmd.append(report_path)
 
-            # cmd.append('--export-json')
-            # cmd.append(json_project_path)
+            if verbosity == 2:
+                msg = 'generated command: ' + ' '.join(cmd)
+                obj_display.display(msg_p=msg, level_p=level2, color_p='green')
 
             subprocess.run(cmd)
-            continue
