e6ab02c87780d2fe653fb267d4d1aeee84651de0
KenjiDeLaRosa <kenji.delarosa@alten.com>
2022-11-18 14:33:34 +0100

    lib: (common): system_fpasim:  rename column: opal_kelly_tag -> reg_id

--- lib/common/common/test/system_fpasim_top_data_gen.py
+++ lib/common/common/test/system_fpasim_top_data_gen.py
@@ -214,7 +214,7 @@ class SystemFpasimTopDataGen:
         description_list.append("trig_out: opal_kelly_type=12")
         description = ', '.join(description_list)
 
-        fid.write("opal_kelly_tag")
+        fid.write("reg_id")
         fid.write(csv_separator)
         fid.write("opal_kelly_addr_hex")
         fid.write(csv_separator)
@@ -229,74 +229,6 @@ class SystemFpasimTopDataGen:
         vunit_conf_obj = self.vunit_conf_obj
         msg0 = 'SystemFpasimTopDataGen._run: ram data generation from files'
         display_obj.display_subtitle(msg_p=msg0,level_p=level0)
-
-        # get Ram data from files
-        ##################################################
-        # tag_list = []
-        # addr_list = []
-        # data_list = []
-        # cmt_list = []
-        # for dic in ram_dic_sequence:
-        #     name = dic['name']
-        #     offset_addr = dic['offset_addr']
-        #     input_filename = dic['input_filename']
-        #     input_filepath = vunit_conf_obj.get_data_filepath(filename_p=input_filename,level_p=level1)
-
-        #     msg0 = 'input_filepath=' + input_filepath
-        #     display_obj.display(msg_p=msg0,level_p=level2)
-
-        #     fid = open(input_filepath,'r')
-        #     lines = fid.readlines()
-        #     fid.close()
-        #     # delete the header
-        #     del lines[0]
-
-        #     for line in lines:
-        #         str_addr,str_value = line.split(csv_separator)
-        #         str_addr = str_addr.replace('\n','')
-        #         str_value = str_value.replace('\n','')
-
-        #         # add the address offset
-        #         addr = int(str_addr) + int(offset_addr,16)
-        #         str_addr = '{0:04x}'.format(addr)
-        #         str_data = '{0:04x}'.format(int(str_value))
-        #         str_result = str_addr + str_data
-              
-        #         data_list.append(str_result)
-        #         cmt_list.append(name)
-        #         addr_list.append("80")
-        #         tag_list.append("0")
-        
-        # L = len(data_list)
-
-    
-        # msg0 = '\nTotal data RAMS= ' + str(L)
-        # display_obj.display(msg_p=msg0,level_p=level1)
-
-        def get_type(usb_type_p,mode_p):
-            result = 0
-            if usb_type_p == 'pipe':
-                if mode_p == 'wr':
-                    result = 0
-                else:
-                    result =  10
-            elif usb_type_p == 'wire':
-                if mode_p == 'wr':
-                    result =  1
-                else:
-                    result = 11
-            elif usb_type_p == 'trig':
-                if mode_p == 'wr':
-                    result = 2
-                else:
-                    result = 22
-            elif usb_type_p == 'nop':
-                result = 9
-            else: 
-                result = -1
-
-            return result
-
 
         def compute_reg_from_field(dic_usb_p,value_list_p):
 
@@ -350,54 +282,55 @@ class SystemFpasimTopDataGen:
             return result
             
 
-        tag_list = []
+        reg_id_list = []
         addr_list = []
         data_list = []
         cmt_list = []
         for cmd in cmd_list:
-            print('cmd',cmd)
+            msg0 = 'SystemFpasimTopDataGen._run: Generate the cmd: '+ cmd
+            display_obj.display(msg_p=msg0,level_p=level1)
 
             cmd_name,mode,*value_list = cmd.split(';')
 
             key = '@'+cmd_name.lower()
-            dic_usb = def_dic.get(key)
-            if dic_usb is None:
-                msg0 = 'SystemFpasimTopDataGen._run: ERROR: the ley ' + str(key) + " doesn't exist ("+cmd+')'
+            dic_cmd = def_dic.get(key)
+            if dic_cmd is None:
+                msg0 = 'SystemFpasimTopDataGen._run: ERROR: the key ' + str(key) + " doesn't exist ("+cmd+')'
                 display_obj.display(msg_p=msg0,level_p=level1,color_p='red')
                 continue
      
             if key == "@nop":
-                usb_type = dic_usb["usb_type"]
-                str_tag = str(get_type(usb_type_p=usb_type,mode_p=mode))
+                reg_id = str(dic_cmd[mode]["reg_id"])
                 usb_addr = "FF"
-                tag_list.append(str_tag)
+                reg_id_list.append(reg_id)
                 addr_list.append(usb_addr)
                 cmt_list.append('"'+cmd+'"')
 
-                result = compute_reg_from_field(dic_usb_p=dic_usb,value_list_p=value_list)
+                result = compute_reg_from_field(dic_usb_p=dic_cmd,value_list_p=value_list)
                 str_result = '{0:08x}'.format(result)
                 data_list.append(str_result)
 
-            elif key == "@usb_ram":
-                usb_type = dic_usb["usb_type"]
-                dic_mode = dic_usb.get(mode)
-                if dic_mode is None:
-                    msg0 = 'SystemFpasimTopDataGen._run: ERROR: the mode ' + str(mode) + " doesn't exist ("+cmd+')'
-                    display_obj.display(msg_p=msg0,level_p=level1,color_p='red')
-                    continue
+            elif key == "@usb_ram":                
+                dic_field = dic_cmd["field"]
 
-                str_tag = str(get_type(usb_type_p=usb_type,mode_p=mode))
-
-                usb_addr = dic_mode["usb_addr"]
-                usb_addr = '{0:02x}'.format(int(usb_addr,16))
-                
                 for value in value_list:
                     new_cmd = cmd_name+';'+mode+';'+value
-                    dic_field = dic_usb["field"]
                     dic = dic_field[value]
+
+                    dic_mode = dic.get(mode)
+                    if dic_mode is None:
+                        msg0 = 'SystemFpasimTopDataGen._run: ERROR: the mode ' + str(mode) + " doesn't exist ("+cmd+')'
+                        display_obj.display(msg_p=msg0,level_p=level1,color_p='red')
+                        continue
+
+                    reg_id = str(dic_mode["reg_id"])
+                    usb_addr = dic_mode["usb_addr"]
+                    usb_addr = '{0:02x}'.format(int(usb_addr,16))
                     name = dic['name']
                     offset_addr = dic['offset_addr']
                     input_filename = dic['input_filename']
+                    ref_output_tb_filename = dic['output_tb_filename']
+                    ref_output_tb_filepath = str(Path(tb_input_base_path,ref_output_tb_filename))
                     input_filepath = vunit_conf_obj.get_data_filepath(filename_p=input_filename,level_p=level1) 
 
                     msg0 = 'input_filepath=' + input_filepath
@@ -408,92 +341,77 @@ class SystemFpasimTopDataGen:
                     fid_tmp.close()
                     # delete the header
                     del lines[0]    
-
+                    addr_ref_list = []
+                    data_ref_list = []
                     for line in lines:
                         str_addr,str_value = line.split(csv_separator)
                         str_addr = str_addr.replace('\n','')
                         str_value = str_value.replace('\n','')  
 
                         # add the address offset
-                        addr = int(str_addr) + int(offset_addr,16)
-                        str_addr = '{0:04x}'.format(addr)
+                        new_addr = int(str_addr) + int(offset_addr,16)
+                        str_addr = '{0:04x}'.format(new_addr)
                         str_data = '{0:04x}'.format(int(str_value))
                         str_result = str_addr + str_data
-                      
 
+                        #####################################
+                        # build lists for the command file
+                        #####################################
                         data_list.append(str_result)
-                        tag_list.append(str_tag)
+                        reg_id_list.append(reg_id)
                         addr_list.append(usb_addr)
                         cmt_list.append('"'+new_cmd+'"')
-               
+
+                        ######################################
+                        # build list as reference file
+                        ######################################
+                        addr_ref_list.append(str(new_addr))
+                        data_ref_list.append(str_value)
+
+
+                    ref_fid = open(ref_output_tb_filepath,'w')
+                    #########################################
+                    # build header file
+                    ref_fid.write('addr_uint')
+                    ref_fid.write(csv_separator)
+                    ref_fid.write('data_uint')
+                    ref_fid.write('\n')
+                    L_ref = len(data_ref_list)
+                    index_max = L_ref - 1
+                    for j in range(0,L_ref):
+                        addr_ref = addr_ref_list[j]
+                        data_ref = data_ref_list[j]
+                        ref_fid.write(addr_ref)
+                        ref_fid.write(csv_separator)
+                        ref_fid.write(data_ref)
+                        if j != index_max:
+                            ref_fid.write('\n')
+                    ref_fid.close()
+
+
                 
             else:
                 # manage trig/wire
                 
-                usb_type = dic_usb["usb_type"]
-                dic_mode = dic_usb.get(mode)
+                dic_mode = dic_cmd.get(mode)
                 if dic_mode is None:
                     msg0 = 'SystemFpasimTopDataGen._run: ERROR: the mode ' + str(mode) + " doesn't exist ("+cmd+')'
                     display_obj.display(msg_p=msg0,level_p=level1,color_p='red')
                     continue
-                str_tag = str(get_type(usb_type_p=usb_type,mode_p=mode))
+                reg_id = str(dic_cmd[mode]["reg_id"])
                 usb_addr = dic_mode["usb_addr"]
                 usb_addr = '{0:02x}'.format(int(usb_addr,16))
-                tag_list.append(str_tag)
+                reg_id_list.append(reg_id)
                 addr_list.append(usb_addr)
                 cmt_list.append('"'+cmd+'"')
 
-                result = compute_reg_from_field(dic_usb_p=dic_usb,value_list_p=value_list)
+                result = compute_reg_from_field(dic_usb_p=dic_cmd,value_list_p=value_list)
       
-                # # work on a deep copy of the field in order to not modify the source dictionnary for further cmd processing
-                # dic_field = copy.deepcopy(dic_usb["field"])
-
-                # for item in value_list:
-                #     field_name_tmp, str_new_value = item.split('=')
-                #     # convert str to integer
-                #     new_value = int(str_new_value)
-                #     # test if the field exist
-                #     dic_bit_field = dic_field.get(field_name_tmp)
-                #     if dic_bit_field is None:
-                #         msg0 = 'SystemFpasimTopDataGen._run: ERROR: the field ' + str(field_name_tmp) + " doesn't exist ("+cmd+')'
-                #         display_obj.display(msg_p=msg0,level_p=level1,color_p='red')
-                #     else:
-                #         # overwrite the value 
-                #         # print('field_name_tmp',field_name_tmp)
-                #         # print(dic_bit_field)
-                #         dic_bit_field['value'] = new_value
-
-                # # compute the register value
-                # result = 0
-                # for key in dic_field.keys():
-                #     dic = dic_field[key]
-
-                #     bit_pos_min = dic['bit_pos_min']
-                #     width = dic['width']
-                #     value = dic['value']
-                #     # test the range of value
-                #     max_value = 2**width - 1
-                #     if value > max_value:
-                #         msg0 = 'SystemFpasimTopDataGen._run: ERROR: the field ' + str(key) + " value is out of range ("+cmd+')'
-                #         display_obj.display(msg_p=msg0,level_p=level1,color_p='red')
-
-                #     # test if the field range is not bigger than the register width
-                #     reg_bit_pos_max = data_width - 1
-                #     bit_pos_max = bit_pos_min + width
-
-                #     if bit_pos_max > reg_bit_pos_max:
-                #         msg0 = 'SystemFpasimTopDataGen._run: ERROR: the field ' + str(key) + " range is out of range ("+cmd+')'
-                #         display_obj.display(msg_p=msg0,level_p=level1,color_p='red')
-                #         msg0 = 'the field ' + str(key) + " ("+str(bit_pos_max)+','+str(bit_pos_min)+') > register width ('+str(reg_bit_pos_max)+',0)'
-                #         display_obj.display(msg_p=msg0,level_p=level2,color_p='red')
-
-                #     result += (value << bit_pos_min)
-
                 str_result = '{0:08x}'.format(result)
                 data_list.append(str_result)
 
                         
-        print('len(tag_list)',len(tag_list))
+        print('len(reg_id_list)',len(reg_id_list))
         print('len(addr_list)',len(addr_list))
         print('len(data_list)',len(data_list))
         print('len(cmt_list)',len(cmt_list))
@@ -501,11 +419,11 @@ class SystemFpasimTopDataGen:
         L = len(data_list)
         index_max = L - 1
         for i in range(L):
-            str_tag = tag_list[i]
+            reg_id = reg_id_list[i]
             str_addr = addr_list[i]
             str_data = data_list[i]
             str_cmt = cmt_list[i]
-            fid.write(str_tag)
+            fid.write(reg_id)
             fid.write(csv_separator)
             fid.write(str_addr)
             fid.write(csv_separator)
