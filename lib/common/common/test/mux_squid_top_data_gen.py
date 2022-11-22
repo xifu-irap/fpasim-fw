58d97dca6c6fac8453243b4f3333981422f6240a
KenjiDeLaRosa <kenji.delarosa@alten.com>
2022-11-22 07:49:52 +0100

    lib (mux_squid):   . add files (mux_squid_model)   . update code

--- lib/common/common/test/mux_squid_top_data_gen.py
+++ lib/common/common/test/mux_squid_top_data_gen.py
@@ -37,6 +37,7 @@
 from common import Display
 from common import ValidSequencer
 from common import TesSignallingModel
+from common import MuxSquidModel
 from pathlib import Path,PurePosixPath
 import json
 import os
@@ -353,12 +354,15 @@ class MuxSquidTopDataGen:
         display_obj.display_subtitle(msg_p=msg0,level_p=level0)
 
         dic_sequence = []
-        dic_sequence.append(json_data["ram1"]["value"])
-        dic_sequence.append(json_data["ram2"]["value"])
+        dic_sequence.append(json_data["ram1"])
+        dic_sequence.append(json_data["ram2"])
+
+        dic_ram_content_tmp = {}
 
         for dic in dic_sequence:
-            input_filename = dic['input_filename']
-            output_filename = dic['output_filename']
+            input_filename = dic["value"]['input_filename']
+            output_filename = dic["value"]['output_filename']
+            name            = dic["generic"]['name']
             output_filepath = str(Path(tb_input_base_path,output_filename)) 
             input_filepath = vunit_conf_obj.get_data_filepath(filename_p=input_filename,level_p=level1)
 
@@ -368,6 +372,62 @@ class MuxSquidTopDataGen:
             display_obj.display(msg_p=msg0,level_p=level2)
 
             shutil.copyfile(input_filepath,output_filepath)
+
+            #####################################################
+            # extract the ram contents in order to compute the expected value 
+            # we assume the data are string representation of integer value
+            #####################################################
+            data_list = []
+            fid_tmp = open(input_filepath,'r')
+            lines = fid_tmp.readlines()
+            fid_tmp.close()
+
+            L = len(lines)
+
+            for i in range(L):
+                line = lines[i]
+                str_addr,str_data = line.split(csv_separator)
+                # skip the header
+                if i != 0:
+                    data_list.append(int(str_data)) 
+
+            # save the ram content
+            dic_ram_content_tmp[name] = data_list
+
+
+        #########################################################
+        # compute the expected output
+        #########################################################
+        mux_squid_offset_name = json_data["ram1"]["generic"]["name"]
+        mux_squid_tf_name = json_data["ram2"]["generic"]["name"]
+        obj = MuxSquidModel()
+        obj.set_ram_mux_squid_offset(data_list_p=dic_ram_content_tmp[mux_squid_offset_name])
+        obj.set_ram_mux_squid_tf(data_list_p=dic_ram_content_tmp[mux_squid_tf_name])
+        obj.set_pixel_id(data_list_p=pixel_id_list)
+        obj.set_data(data_list_p=pixel_result_oversample_list)
+        obj.set_mux_squid_feedback(data_list_p=mux_squid_oversample_list)
+        obj.run()
+
+        result_list = obj.get_result()
+
+        output_filename = json_data["model"]["value"]["output_filename"]
+        output_filepath = str(Path(tb_input_base_path_p,output_filename))
+        fid = open(output_filepath,'w')
+        # write header
+        fid.write("expected_output_int")
+        fid.write('\n')
+        L = len(result_list)
+        index_max = L - 1
+        for index in range(L):
+            result = result_list[index]
+            fid.write(str(result))
+            if index != index_max:
+                fid.write('\n')
+
+        fid.close()
+
+
+
 
 
         return None
