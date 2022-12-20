8e39a200e085f78e09fdc362d174388d16807bf5
k.dlr <kenji.delarosa@alten.com>
2022-12-20 14:43:42 +0100

    clock: beautify the code

--- src/hdl/clocking/clocking_top.vhd
+++ src/hdl/clocking/clocking_top.vhd
@@ -37,8 +37,8 @@ entity clocking_top is
     ---------------------------------------------------------------------
     -- input
     ---------------------------------------------------------------------
-    i_clk_p   : in  std_logic;          -- differential clock p @250MHz
-    i_clk_n   : in  std_logic;          -- differential clock n @250MHZ
+    i_clk_p : in std_logic;             -- differential clock p @250MHz
+    i_clk_n : in std_logic;             -- differential clock n @250MHZ
 
     ---------------------------------------------------------------------
     -- output
@@ -49,50 +49,55 @@ entity clocking_top is
     o_clk     : out std_logic;          -- sys output clock @333.33333 MHz
 
     o_locked : out std_logic
-  );
+    );
 end entity clocking_top;
 
 architecture RTL of clocking_top is
 
-component fpasim_clk_wiz_0
-port
- (-- Clock in ports
-  -- Clock out ports
-  clk_out1          : out    std_logic;
-  clk_out2          : out    std_logic;
-  clk_out3          : out    std_logic;
-  clk_out4          : out    std_logic;
-  -- Status and control signals
-  locked            : out    std_logic;
-  clk_in1_p         : in     std_logic;
-  clk_in1_n         : in     std_logic
- );
-end component;
+  component fpasim_clk_wiz_0
+    port
+      (                                 -- Clock in ports
+        -- Clock out ports
+        clk_out1  : out std_logic;
+        clk_out2  : out std_logic;
+        clk_out3  : out std_logic;
+        clk_out4  : out std_logic;
+        -- Status and control signals
+        locked    : out std_logic;
+        clk_in1_p : in  std_logic;
+        clk_in1_n : in  std_logic
+        );
+  end component;
 
   signal adc_clk : std_logic;
   signal ref_clk : std_logic;
   signal dac_clk : std_logic;
-  signal clk : std_logic;
-  signal locked : std_logic;
+  signal clk     : std_logic;
+  signal locked  : std_logic;
 
 begin
 
   inst_fpasim_clk_wiz_0 : fpasim_clk_wiz_0
     port map(
       -- Clock out ports  
-      clk_out1  => adc_clk,           -- output clock @250MHz
-      clk_out2  => ref_clk,           -- output clock @62.5MHz
-      clk_out3  => dac_clk,           -- output clock @500MHz
-      clk_out4  => clk,               -- output clock @333.33333MHz
+      clk_out1  => adc_clk,             -- output clock @250MHz
+      clk_out2  => ref_clk,             -- output clock @62.5MHz
+      clk_out3  => dac_clk,             -- output clock @500MHz
+      clk_out4  => clk,                 -- output clock @333.33333MHz
       -- Status and control signals                
       locked    => locked,
       -- Clock in ports
       clk_in1_p => i_clk_p,
       clk_in1_n => i_clk_n
-    );
+      );
+
+    ---------------------------------------------------------------------
+    -- output
+    ---------------------------------------------------------------------
     o_adc_clk <= adc_clk;
     o_ref_clk <= ref_clk;
     o_dac_clk <= dac_clk;
-    o_clk <= clk;
-    o_locked <= locked;
+    o_clk     <= clk;
+    o_locked  <= locked;
+    
 end architecture RTL;
