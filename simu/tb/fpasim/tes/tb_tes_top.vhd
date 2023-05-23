1f8f727bdc13efefde2708c8afe812c7f2d43094
KenjiDeLaRosa <kenji.delarosa@alten.com>
2023-05-23 13:49:44 +0200

    ci: (tes) minor update.

--- simu/tb/fpasim/tes/tb_tes_top.vhd
+++ simu/tb/fpasim/tes/tb_tes_top.vhd
@@ -17,13 +17,15 @@
 --                              along with this program.  If not, see <https://www.gnu.org/licenses/>.
 -- -------------------------------------------------------------------------------------------------------------
 --    email                   kenji.delarosa@alten.com
---!   @file                   tb_tes_top.vhd 
+--    @file                   tb_tes_top.vhd 
 -- -------------------------------------------------------------------------------------------------------------
 --    Automatic Generation    No
 --    Code Rules Reference    SOC of design and VHDL handbook for VLSI development, CNES Edition (v2.1)
 -- -------------------------------------------------------------------------------------------------------------
---!   @details                
+--    @details                
 --
+--     Testbench of the tes_top module.
+-- 
 -- -------------------------------------------------------------------------------------------------------------
 
 library ieee;
@@ -145,7 +147,7 @@ architecture simulate of tb_tes_top is
   -- errors/status
   ---------------------------------------------------------------------
   signal o_errors : std_logic_vector(15 downto 0);
-  signal o_status : std_logic_vector(7 downto 0);  -- @suppress "signal o_status is never read"
+  signal o_status : std_logic_vector(7 downto 0); 
 
   ---------------------------------------------------------------------
   -- Clock definition
@@ -159,7 +161,7 @@ architecture simulate of tb_tes_top is
   signal reg_start      : std_logic := '0';
   signal reg_rd_valid   : std_logic := '0';
   signal reg_gen_finish : std_logic := '0';
-  signal reg_valid      : std_logic;  -- @suppress "signal reg_valid is never read"
+  signal reg_valid      : std_logic;  
 
   -- Cmd
   signal cmd_start      : std_logic := '0';
@@ -172,7 +174,7 @@ architecture simulate of tb_tes_top is
   signal data_gen_finish        : std_logic := '0';
   signal data_valid             : std_logic := '0';
   signal data_count_in          : std_logic_vector(31 downto 0);
-  signal data_count_overflow_in : std_logic;  -- @suppress "signal data_count_overflow_in is never read"
+  signal data_count_overflow_in : std_logic; 
 
   -- ram tes pulse shape
   signal ram1_wr_start      : std_logic                    := '0';
@@ -180,7 +182,7 @@ architecture simulate of tb_tes_top is
   signal ram1_rd_valid      : std_logic                    := '0';
   signal ram1_wr_gen_finish : std_logic                    := '0';
   signal ram1_rd_gen_finish : std_logic                    := '0';
-  signal ram1_error         : std_logic_vector(0 downto 0) := (others => '0');  -- @suppress "signal ram1_error is never read"
+  signal ram1_error         : std_logic_vector(0 downto 0) := (others => '0');  
 
   -- ram tes steady state
   signal ram2_wr_start      : std_logic                    := '0';
@@ -188,14 +190,14 @@ architecture simulate of tb_tes_top is
   signal ram2_rd_valid      : std_logic                    := '0';
   signal ram2_wr_gen_finish : std_logic                    := '0';
   signal ram2_rd_gen_finish : std_logic                    := '0';
-  signal ram2_error         : std_logic_vector(0 downto 0) := (others => '0');  -- @suppress "signal ram2_error is never read"
+  signal ram2_error         : std_logic_vector(0 downto 0) := (others => '0');  
 
   -- check
   signal data_count_out          : std_logic_vector(31 downto 0);
-  signal data_count_overflow_out : std_logic;  -- @suppress "signal data_count_overflow_out is never read"
+  signal data_count_overflow_out : std_logic; 
 
   signal data_stop      : std_logic := '0';
-  signal data_out_error : std_logic_vector(0 downto 0);  -- @suppress "signal data_out_error is never read"
+  signal data_out_error : std_logic_vector(0 downto 0); 
 
   ---------------------------------------------------------------------
   -- filepath definition
@@ -244,13 +246,13 @@ architecture simulate of tb_tes_top is
   -- VUnit Scoreboard objects
   ---------------------------------------------------------------------
   -- loggers 
-  constant c_LOGGER_SUMMARY          : logger_t  := get_logger("log:summary");  -- @suppress "Expression does not result in a constant"
+  constant c_LOGGER_SUMMARY          : logger_t  := get_logger("log:summary"); 
   -- checkers
-  constant c_CHECKER_ERRORS          : checker_t := new_checker("check:errors");  -- @suppress "Expression does not result in a constant"
-  constant c_CHECKER_DATA_COUNT      : checker_t := new_checker("check:data_count");  -- @suppress "Expression does not result in a constant"
-  constant c_CHECKER_RAM1            : checker_t := new_checker("check:ram1:ram_" & g_RAM1_NAME);  -- @suppress "Expression does not result in a constant"
-  constant c_CHECKER_RAM2            : checker_t := new_checker("check:ram2:ram_" & g_RAM2_NAME);  -- @suppress "Expression does not result in a constant"
-  constant c_CHECKER_DATA            : checker_t := new_checker("check:out:data_out");  -- @suppress "Expression does not result in a constant"
+  constant c_CHECKER_ERRORS          : checker_t := new_checker("check:errors"); 
+  constant c_CHECKER_DATA_COUNT      : checker_t := new_checker("check:data_count"); 
+  constant c_CHECKER_RAM1            : checker_t := new_checker("check:ram1:ram_" & g_RAM1_NAME);
+  constant c_CHECKER_RAM2            : checker_t := new_checker("check:ram2:ram_" & g_RAM2_NAME);
+  constant c_CHECKER_DATA            : checker_t := new_checker("check:out:data_out"); 
 
 begin
 
@@ -281,7 +283,7 @@ begin
     ---------------------------------------------------------------------
     -- VUNIT - Scoreboard object : Visibility definition
     ---------------------------------------------------------------------
-    if g_VUNIT_DEBUG = true then  -- @suppress "Redundant boolean equality check with true"
+    if g_VUNIT_DEBUG = true then  
       -- the simulator doesn't stop on errors => stop on failure
       set_stop_level(failure);
     end if;
@@ -295,6 +297,7 @@ begin
     if g_RAM2_VERBOSITY > 0 then
       show(get_logger("check:ram2"), display_handler, pass);
     end if;
+    --show(get_logger("check:out:data_out"), display_handler, pass);
 
     pkg_wait_nb_rising_edge_plus_margin(i_clk, i_nb_rising_edge => 1, i_margin => 12 ps);
 
@@ -393,17 +396,11 @@ begin
     wait until rising_edge(i_clk) and cmd_gen_finish = '1';
     pkg_wait_nb_rising_edge_plus_margin(i_clk, i_nb_rising_edge => 1, i_margin => 12 ps);
 
-    ---------------------------------------------------------------------
-    -- Data Generation
-    ---------------------------------------------------------------------
-    info("Start data Generation");
-    data_start <= '1';
-    pkg_wait_nb_rising_edge_plus_margin(i_clk, i_nb_rising_edge => 1, i_margin => 12 ps);
 
     ---------------------------------------------------------------------
     -- RAM Check: RAM1
     ---------------------------------------------------------------------
-    if g_RAM1_CHECK = true then  -- @suppress "Redundant boolean equality check with true"
+    if g_RAM1_CHECK = true then  
       info("Start RAM reading: " & g_RAM1_NAME);
       ram1_rd_start <= '1';
       pkg_wait_nb_rising_edge_plus_margin(i_clk, i_nb_rising_edge => 1, i_margin => 12 ps);
@@ -414,7 +411,7 @@ begin
     ---------------------------------------------------------------------
     -- RAM Check: RAM2
     ---------------------------------------------------------------------
-    if g_RAM2_CHECK = true then  -- @suppress "Redundant boolean equality check with true"
+    if g_RAM2_CHECK = true then
       info("Start RAM reading: " & g_RAM2_NAME);
       ram2_rd_start <= '1';
       pkg_wait_nb_rising_edge_plus_margin(i_clk, i_nb_rising_edge => 1, i_margin => 12 ps);
@@ -422,6 +419,12 @@ begin
       wait until rising_edge(i_clk) and ram2_rd_gen_finish = '1';
     end if;
 
+    ---------------------------------------------------------------------
+    -- Data Generation
+    ---------------------------------------------------------------------
+    info("Start data Generation");
+    data_start <= '1';
+    pkg_wait_nb_rising_edge_plus_margin(i_clk, i_nb_rising_edge => 1, i_margin => 12 ps);
     ---------------------------------------------------------------------
     -- Wait end of input data generation
     ---------------------------------------------------------------------
@@ -432,7 +435,7 @@ begin
         v_test := 1;
       end if;
       if o_pixel_valid = '1' and o_frame_sof = '1' then
-        info("Frame"&to_string(to_integer(unsigned(o_frame_id))));
+        info("o_frame_id: "&to_string(to_integer(unsigned(o_frame_id))));
       end if;
       pkg_wait_nb_rising_edge_plus_margin(i_clk, i_nb_rising_edge => 1, i_margin => 12 ps);
     end loop;
@@ -739,7 +742,7 @@ begin
   -- Input: data generation
   ---------------------------------------------------------------------
   gen_data : if true generate
-    signal vect_tmp : std_logic_vector(0 downto 0);  -- @suppress "signal vect_tmp is never read"
+    signal vect_tmp : std_logic_vector(0 downto 0); 
   begin
 
     -- valid sequence generation
@@ -898,7 +901,7 @@ begin
   ---------------------------------------------------------------------
   -- log: data out
   ---------------------------------------------------------------------
-  gen_log : if g_ENABLE_LOG = true generate  -- @suppress "Redundant boolean equality check with true"
+  gen_log : if g_ENABLE_LOG = true generate  
     signal pixel_sof_vect_tmp : std_logic_vector(0 downto 0);
     signal pixel_eof_vect_tmp : std_logic_vector(0 downto 0);
     signal frame_sof_vect_tmp : std_logic_vector(0 downto 0);
@@ -960,7 +963,7 @@ begin
 
   end generate gen_log;
 
-  gen_log_all : if g_ENABLE_LOG = true generate  -- @suppress "Redundant boolean equality check with true"
+  gen_log_all : if g_ENABLE_LOG = true generate 
     constant c_FILEPATH_DATA_OUT : string := c_OUTPUT_BASEPATH & "vhdl_data_out_all.csv";
     signal pixel_sof_vect_tmp    : std_logic_vector(0 downto 0);
     signal pixel_eof_vect_tmp    : std_logic_vector(0 downto 0);
@@ -1022,7 +1025,7 @@ begin
   ---------------------------------------------------------------------
   -- check data
   ---------------------------------------------------------------------
-  gen_check_data : if g_ENABLE_CHECK = true generate  -- @suppress "Redundant boolean equality check with true"
+  gen_check_data : if g_ENABLE_CHECK = true generate  
   begin
 
     inst_pkg_vunit_data_checker : pkg_vunit_data_checker_1(
