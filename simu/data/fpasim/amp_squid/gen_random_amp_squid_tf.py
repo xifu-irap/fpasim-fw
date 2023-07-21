4257cea62c87da96ea00d65d0afd119873dd1fe0
KenjiDeLaRosa <kenji.delarosa@alten.com>
2023-07-21 11:01:33 +0200

    ci, amp_squid: replace main code by function

--- simu/data/fpasim/amp_squid/gen_random_amp_squid_tf.py
+++ simu/data/fpasim/amp_squid/gen_random_amp_squid_tf.py
@@ -34,6 +34,49 @@
 from pathlib import Path
 import random
 
+def generate_random_int(output_filepath_p,ram_address_width_p,ram_data_width_p):
+    """
+        This function generates random int values
+
+        Parameters
+        ----------
+        output_filepath_p: str
+            output *.csv file
+        ram_address_width_p: in
+            address width of the ram (expressed in bits)
+        ram_data_width_p: int
+            data width of the ram (expressed in bits)
+
+        Returns
+        -------
+            None
+
+    """
+
+    # Number of words in the RAM
+    nb_data = 2**ram_address_width_p
+    # max value in the ram
+    max_value = 2**(ram_data_width_p - 1) - 1
+    # min value in the RAM
+    min_value = -2**(ram_data_width_p - 1)
+
+    index_max = nb_data - 1
+    with open(output_filepath_p,'w') as fid:
+        for index in range(nb_data):
+            if index == 0:
+                fid.write('offset_addr_uint'+str(ram_address_width_p)+'_t')
+                fid.write(csv_separator)
+                fid.write('data_int'+str(ram_data_width_p)+'_t')
+                fid.write('\n')
+            # compute a random integer N such that min_value <= N <= max_value
+            data = random.randint(min_value, max_value)
+            str_line = str(data)
+
+            fid.write(str(index))
+            fid.write(csv_separator)
+            fid.write(str_line)
+            if index != index_max:
+                fid.write('\n')
 
 if __name__ == "__main__":
 
@@ -43,29 +86,7 @@ if __name__ == "__main__":
     base_address = str(Path(__file__).parents[0]) # base path of this script
     # output filename
     filename_out = 'random_amp_squid_tf.csv'
-    # Number of words in the RAM
-    nb_data = 2**14
-    # max value in the ram
-    max_value = 2**15 - 1
-    # min value in the RAM
-    min_value = -2**15
-
+     # compute output filepath
     filepath_out = str(Path(base_address,filename_out))
-    index_max = nb_data - 1
-    with open(filepath_out,'w') as fid:
-        for index in range(nb_data):
-            # write the csv file header
-            if index == 0:
-                fid.write('offset_addr_uint14_t')
-                fid.write(csv_separator)
-                fid.write('data_int16_t')
-                fid.write('\n')
-            # compute a random integer N such that min_value <= N <= max_value
-            data = random.randint(min_value, max_value)
-            str_line = str(data)
-            # write data
-            fid.write(str(index))
-            fid.write(csv_separator)
-            fid.write(str_line)
-            if index != index_max:
-                fid.write('\n')
+
+    generate_random_int(output_filepath_p=filepath_out,ram_address_width_p=14,ram_data_width_p=16)
